3845ed09dc36ef0f4cdb6f8b4529ddd48e66cee5
Adrian <adrpieper@gmail.com>
2017-03-06 20:05:16 +0100

    Dodałem prosty przykład wyświetlania elementów tablicy

---
+++ src/array/SimplePrinter.java
@@ -0,0 +1,15 @@
+package array;
+
+/**
+ * Created by Adrian on 2017-03-06.
+ */
+public class SimplePrinter {
+
+    public static void main(String[] args) {
+        int[] numbers = {10,2,32,4};
+
+        for (int n: numbers) {
+            System.out.println(n);
+        }
+    }
+}
