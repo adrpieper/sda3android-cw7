1f79ecffb110b722183de24f0cb20cedfbadd5ba
Adrian <adrpieper@gmail.com>
2017-03-07 20:48:14 +0100

    Zamieniłem punkt 2 z zadania Stream

--- src/stream/StreamExercises.java
+++ src/stream/StreamExercises.java
@@ -19,10 +19,36 @@ public class StreamExercises {
                 .filter(StreamExercises::isLessThanTen)
                 .collect(Collectors.toList());
         lessThanTenList.forEach(System.out::println);
+        System.out.println();
+
+        List<Integer> dividedByThreeList = number.stream()
+                .filter(StreamExercises::isDividedByThree)
+                .collect(Collectors.toList());
+        dividedByThreeList.forEach(System.out::println);
+        System.out.println();
+
+
+        List<Integer> fiveOnEndList = number.stream()
+                .filter(StreamExercises::isFiveOnEnd)
+                .collect(Collectors.toList());
+        fiveOnEndList.forEach(System.out::println);
+        System.out.println();
     }
 
     public static boolean isLessThanTen(int i) {
         return i < 10;
+    }
+
+    public static boolean isDividedByThree(int i){
+        int reszta = i%3;
+        boolean czyZero = (reszta==0);
+        return czyZero;
+    }
+
+    public static boolean isFiveOnEnd(Integer i){
+        String text = i.toString();
+        char lastChar = text.charAt(text.length()-1);
+        return lastChar == '5';
     }
 
     private static void zad1() {
@@ -33,18 +59,10 @@ public class StreamExercises {
         number.stream().filter(StreamExercises::isLessThanTen).forEach(System.out::println);
         System.out.println();
 
-        number.stream().filter((Integer i) -> {
-            int reszta = i%3;
-            boolean czyZero = (reszta==0);
-            return czyZero;
-        }).forEach(System.out::println);
+        number.stream().filter(StreamExercises::isDividedByThree).forEach(System.out::println);
         System.out.println();
 
-        number.stream().filter((Integer i) -> {
-            String text = i.toString();
-            char lastChar = text.charAt(text.length()-1);
-            return lastChar == '5';
-        }).forEach(System.out::println);
+        number.stream().filter(StreamExercises::isFiveOnEnd).forEach(System.out::println);
         System.out.println();
     }
 }
