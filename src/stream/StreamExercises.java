6fae4cee1dd3945bfa0c66d7548f8c4cc0275997
Adrian <adrpieper@gmail.com>
2017-03-07 20:15:10 +0100

    Wykonałem pierszy punkt ze zadania Stream

--- src/stream/StreamExercises.java
+++ src/stream/StreamExercises.java
@@ -9,14 +9,30 @@ import java.util.List;
 public class StreamExercises {
 
     public static void main(String[] args) {
+        zad1();
+    }
+
+    private static void zad1() {
         List<Integer> number = Arrays.asList(1,25,30,31,23,15,16,3,5,30);
         number.stream().forEach(System.out::println);
+        System.out.println();
+
         number.stream().filter((i) -> i<10).forEach(System.out::println);
+        System.out.println();
+
         number.stream().filter((Integer i) -> {
             int reszta = i%3;
             boolean czyZero = (reszta==0);
             return czyZero;
         }).forEach(System.out::println);
+        System.out.println();
+
+        number.stream().filter((Integer i) -> {
+            String text = i.toString();
+            char lastChar = text.charAt(text.length()-1);
+            return lastChar == '5';
+        }).forEach(System.out::println);
+        System.out.println();
     }
 }
 
