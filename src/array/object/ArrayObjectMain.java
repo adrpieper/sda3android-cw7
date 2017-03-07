7917b644dd1d389f6fe64d8eaea0c53085ade1a7
Adrian <adrpieper@gmail.com>
2017-03-07 17:58:17 +0100

    Ukończyłem program w sposób obiektowy

--- src/array/object/ArrayObjectMain.java
+++ src/array/object/ArrayObjectMain.java
@@ -6,6 +6,17 @@ package array.object;
 public class ArrayObjectMain {
 
     public static void main(String[] args) {
-
+        int [] numbers = {1,5,12,90};
+        ArrayTask arrayTask = new ArrayPrinter();
+        arrayTask.setData(numbers);
+        arrayTask.runTask();
+        ArrayTask annonymousTask = new ArrayTask() {
+            @Override
+            protected void doTask(int i) {
+                System.out.println(i*i);
+            }
+        };
+        annonymousTask.setData(numbers);
+        annonymousTask.runTask();
     }
 }
