124ddd381d6275aa013864cda18b82e475dc6049
Adrian <adrpieper@gmail.com>
2017-03-08 20:23:06 +0100

    Dodałem klasy Person i Car

---
+++ src/format/Car.java
@@ -0,0 +1,30 @@
+package format;
+
+/**
+ * Created by Adrian on 2017-03-08.
+ */
+public class Car {
+    private String brand;
+    private int age;
+
+    public Car(String brand, int age) {
+        this.brand = brand;
+        this.age = age;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
