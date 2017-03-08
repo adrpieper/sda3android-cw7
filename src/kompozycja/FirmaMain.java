e4be98efd2497d115d0d403d467812afb5b105bc
Adrian <adrpieper@gmail.com>
2017-03-08 20:10:03 +0100

    Ukończyłem program z Firmą

--- src/kompozycja/FirmaMain.java
+++ src/kompozycja/FirmaMain.java
@@ -15,21 +15,36 @@ public class FirmaMain {
                 "Kowalski",
                 new Adres("Kwiatowa","Gdańsk",10)
         );
+
         firma.add(pracownik);
+        firma.add(new Pracownik(
+                "Jan",
+                "Dominik",
+                new Adres("Złota","Gdańsk",26)
+        ));
 
-
+        show(firma);
     }
 
     public static void show(Firma firma){
 
+        System.out.println(firma.getNazwa() + "(" + firma.getTyp() + ")");
+        show(firma.getAdres());
+        for (Pracownik pracownik : firma.getPracownicy()) {
+            show(pracownik);
+        }
     }
 
     public static void show(Pracownik pracownik){
-
+        System.out.println(" Pracownik : " +pracownik.getImie() + " " + pracownik.getNazwisko());
+        show(pracownik.getAdres());
     }
 
 
     public static void show(Adres adres){
+        System.out.println("adres : ");
+        System.out.println(adres.getMiasto());
+        System.out.println("ul. " + adres.getUlica() + " " + adres.getNumerDomu());
 
     }
 }
